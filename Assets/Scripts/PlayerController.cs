d42efa5478a09297c4d750e4200a216e49ea8c93
div-antk <51283468+owaii@users.noreply.github.com>
2022-06-15 09:32:13 +0900

    誤っていた記述の修正

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 必要なコンポーネントを定義
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
+
 public class PlayerController : MonoBehaviour
 {
-    // 必要なコンポーネントを定義
-    [RequireComponent(typeof(Rigidbody2D))]
-    [RequireComponent(typeof(CapsuleCollider2D))]
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private int moveSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +21,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // プレイヤーを移動させる処理
-        rb.velocity = new Vector2(Input.GetAxis("Horizintal") * moveSpeed, rb.velocity.y);
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, rb.velocity.y);
     }
 }
