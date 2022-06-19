ce5f1faa228406c3d63e685018da3238a113f0c0
div-antk <51283468+owaii@users.noreply.github.com>
2022-06-19 22:34:20 +0900

    プレイヤーのジャンプの処理を追記

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,7 +9,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
+
     [SerializeField] private int moveSpeed;
+    [SerializeField] private int jumpForce;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +22,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // スペースキーが押された際、かつ落下中ではないときにJump関数を呼ぶ
+        if (Input.GetKeyDown(KeyCode.Space) && !(rb.velocity.y < -0.5f)) {
+            Jump()
+        }
+
         // プレイヤーを移動させる処理
         rb.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, rb.velocity.y);
+    }
+
+    void Jump() {
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 }
